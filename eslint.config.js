dd4373639749bd6a8c49ff5d4622c748fada6d78
Prochmi99 <152851523+MiroslavProchazka@users.noreply.github.com>
2026-06-13 12:33:07 +0200

    chore: scope ESLint rules to src files

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist', 'node_modules'] },
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}'],
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
       ecmaVersion: 2020,
